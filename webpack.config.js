f1646651465cc66b8bddb62d16105c47d1ee314a
Michael <mddally@gmail.com>
2021-09-21 10:52:36 -0400

    Formatting and styling

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,18 @@ module.exports = {
 	output: {
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'dist')
-	}
-}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource'
+			}
+		],
+
+	},
+};
